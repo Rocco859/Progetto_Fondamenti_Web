29bc5b45f1c53b00adc096e4108e719f0070eac2
Andreadg171 <a.digiacomo@studenti.poliba.it>
2026-08-30 12:20:37 +0200

    rimozione timestamp

--- fridgy-backend/models/User.js
+++ fridgy-backend/models/User.js
@@ -6,6 +6,6 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     frigo: { type: Array, default: [] }
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model('User', userSchema);
